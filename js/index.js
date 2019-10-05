3b7081e83ed6ed9bed6201b139c436a64a3e3467
Regbmiller <40448656+Regbmiller@users.noreply.github.com>
2019-10-04 21:34:03 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+
+//1
+const navLink = document.querySelector('nav-link');
+    //console.log(navLink);
+
+navLink.addEventListener('dblclick', (e) => {
+  e.target.style.backgroundColor = 'red';
+});
+
+//2
+const navLink = document.querySelector('nav-link');
+    //console.log(navLink);
+
+trade.addEventListener('dblclick', (e) => {
+  e.target.style.backgroundColor = 'green';
+  e.stopPropagation();
+});
+
+//3 
+
+//4
+
+//5
+
+//6
